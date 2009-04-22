05ffd8b73631e515a06456cb3cfa2894e6f93854
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2009-04-22 20:07:08 +0000

    Proper packet handling Broadcast/Multicast addresses
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@51 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- firmware/lib/packet.c
+++ firmware/lib/packet.c
@@ -7,6 +7,7 @@
 #include "packet.h"
 #include "bus.h"
 #include "frame.h"
+#include "serial_handler.h"
 
 struct frame outframe;
 struct ubpacket inpacket;
@@ -15,33 +16,45 @@ address_t   packet_address = 0x23;
 seq_t       packet_ack_seq;
 
 uint8_t packet_acked = 1;
-uint8_t packet_valid = 0;
 uint8_t packet_incomming;
 uint8_t packet_timeout;
+uint8_t packet_sniff = 0;
+uint8_t packet_fired = 0;
 
-void packet_init(void)
+void packet_init(uint8_t adr)
 {
     bus_init();
     DDRC |= (1<<PC2);
     PORTD |= (1<<PD7);
-
-    if( PIND & (1<<PD7) ){
-        packet_address = 'A';
-    }else{
-        packet_address = 'B';
+    packet_address = adr;
+    if(adr == 0){
+        if( PIND & (1<<PD7) ){
+            packet_address = 'A';
+        }else{
+            packet_address = 'B';
+        }
     }
 }
 
-inline uint8_t packet_isUnicast(struct ubpacket * in)
+inline struct ubpacket * packet_getIncomming(void)
 {
-    return in->flags & UB_PACKET_UNICAST;
+    return &inpacket;
 }
 
 inline uint8_t packet_isBroadcast(struct ubpacket * in)
 {
-    return in->flags & UB_PACKET_BROADCAST;
+    return in->dest == UB_ADDRESS_BROADCAST;
 }
 
+inline uint8_t packet_isMulticast(struct ubpacket * in)
+{
+    return (in->dest & UB_ADDRESS_MULTICAST) && (!packet_isBroadcast(in));
+}
+
+inline uint8_t packet_isUnicast(struct ubpacket * in)
+{
+    return !packet_isBroadcast(in) && !packet_isMulticast(in);
+}
 inline struct ubpacket * packet_getSendBuffer(void)
 {
     return (struct ubpacket *) outframe.data;
@@ -49,9 +62,11 @@ inline struct ubpacket * packet_getSendBuffer(void)
 
 void packet_transmit(struct frame * f)
 {
+//    DEBUG("s");
     struct ubpacket *out = (struct ubpacket *)(f->data);
     f->len = out->len+UB_PACKET_HEADER;
     bus_send(f,1);
+//    DEBUG("S");
 }
 
 void packet_send(void)
@@ -62,17 +77,22 @@ void packet_send(void)
     out->src = packet_address;
     packet_acked = 0;
     packet_transmit(&outframe);
+    packet_fired = 1;
 }
 
 uint8_t packet_done(void)
 {
-    return packet_acked;
+    if(packet_fired && packet_acked){
+        packet_fired = 0;
+        return 1;
+    }
+    return 0;
 }
 
 void packet_ack(struct ubpacket * p)
 {
     struct frame f;
-    struct ubpacket *out = f.data;
+    struct ubpacket *out = (struct ubpacket *) f.data;
     out->flags = UB_PACKET_ACK;
     out->seq = p->seq;
     out->len = 0;
@@ -96,36 +116,76 @@ inline void packet_processed(void)
     packet_incomming = 0;
 }
 
+#define MAX_HOSTS   2
+struct {
+    address_t src;
+    seq_t     seq;
+}seqs[MAX_HOSTS];
+
+//checks if seq was seen before. A round-robin table is used to
+//keep track of the sequence numbers.
+//returns 1 if the packet is new
+uint8_t packet_checkseq(struct ubpacket * p, uint8_t update)
+{
+    static uint8_t seqrr = 0;
+    uint8_t i;
+    uint8_t ret;
+    address_t src = p->src;
+    seq_t seq = p->seq;
+
+    for(i=0;i<MAX_HOSTS;i++){
+        if(seqs[i].src == src){                 //host known
+            ret = seqs[i].seq!=seq?1:0;       //check for old seq
+            if(update)
+                seqs[i].seq = seq;
+                                    //TODO: add some real seq counting here
+            return ret;
+        }
+    }                                           //host unknown
+    if(!update)
+        return 1;
+    seqs[seqrr].src = src;                      //add host to table
+    seqs[seqrr++].seq = seq;
+    if(seqrr == MAX_HOSTS)                      //round robin style
+        seqrr = 0;
+    return 1;
+}
+
+
 void packet_process(struct ubpacket * in)
 {
     if(packet_incomming)            //There is still an unprocessed packet
                                     //in the buffer
         return;                     //Just wait for the other end to retry
-
-    if(!packet_acked)        //guarantee the userland that the buffer
-    {                        //is empty then a new packet arrives
-        if(inpacket.seq == in->seq)     //this ack was lost
+    if(packet_sniff){
+        memcpy(&inpacket,in,sizeof(inpacket));
+        packet_incomming = 1;                   //mark packet as new
+    }else if(!packet_acked){        //guarantee the userland that the buffer
+                              //is empty when a new packet arrives
+        if( !packet_checkseq(in,0) )    //this ack was lost
             packet_ack(in);             //send it again
+        //this packet is ignored.
+        //the sender will try again.
     }else if( packet_isUnicast(in) ){
+        //DEBUG("unicast");
         packet_ack(in);
-        if(inpacket.seq != in->seq || !packet_valid){    //avoid receiving duplicates
+//        if(inpacket.seq != in->seq || !packet_valid){    //avoid receiving duplicates
+        if( packet_checkseq(in,1)){
             PORTB |= (1<<PB0);
-                                    //TODO: add some real seq counting here
             memcpy(&inpacket,in,sizeof(inpacket));
             packet_incomming = 1;                   //mark packet as new
-            packet_valid = 1;
             PORTB &= ~(1<<PB0);
         }
     }else if( packet_isBroadcast(in) ){
+//        DEBUG("broadcast");
         memcpy(&inpacket,in,sizeof(in));
         packet_incomming = 1;                   //mark packet as new
-        packet_valid = 1;
     }
 }
 
 void packet_in(struct ubpacket * in)
 {
-    if( packet_isLocal(in->dest) ){
+    if( packet_isLocal(in->dest) || packet_sniff){
         //Is this an ack packet for us?
         if( in->flags & UB_PACKET_ACK && in->seq == packet_ack_seq){
             packet_ack_seq++;
@@ -141,26 +201,28 @@ void packet_in(struct ubpacket * in)
 
 void packet_tick(void)
 {
-    struct frame *inframe; //= NULL;
-    PORTC |= (1<<PC2);
+    //struct frame *inframe; //= NULL;
+    //PORTC |= (1<<PC2);
     struct ubpacket * in;
     bus_tick();
     if(bus_receive() ){ //&& inframe == NULL){
         //Keep track of the frame
-        inframe = bus_frame;                 //no cli needed
+        //inframe = bus_frame;                 //no cli needed
         //cast the data part into a packet
+        //DEBUG("I");
         in = (struct ubpacket *) bus_frame->data;
-        if(in->len > (inframe->len-UB_PACKET_HEADER)){
+        if( (in->len + UB_PACKET_HEADER) > bus_frame->len ){
             //discard
         }else{
             packet_in(in);
         }
-        inframe->isnew = 0;
+        //inframe->isnew = 0;
         //inframe = NULL;
+        bus_frame->isnew = 0;
     }
     if(!packet_acked && packet_timeout && --packet_timeout==0){
         packet_send();
     }
-    PORTC &= ~(1<<PC2);
+    //PORTC &= ~(1<<PC2);
 }
 
--- firmware/lib/packet.h
+++ firmware/lib/packet.h
@@ -20,23 +20,31 @@ struct ubpacket{
 #define UB_PACKET_HEADER        (sizeof(struct ubpacket) - FRAME_MAX)
 
 #define UB_PACKET_ACK           1
-#define UB_PACKET_UNICAST       2
-#define UB_PACKET_BROADCAST     4
-#define UB_PACKET_MULTICAST     8
+//#define UB_PACKET_UNICAST       2
+//#define UB_PACKET_BROADCAST     4
+//#define UB_PACKET_MULTICAST     8
+
+
+#define UB_ADDRESS_MULTICAST     (1<<(sizeof(address_t)*8-1))     //first bit is one
+#define UB_ADDRESS_BROADCAST     ((1<<sizeof(address_t)*8)-1)     //all ones
 
 #define PACKET_TIMEOUT          200     //we wait max 200ms for an ack
 
-void packet_init(void);
+void packet_init(uint8_t adr);
 void packet_send(void);
-uint8_t packet_sendstate(void);
+//uint8_t packet_sendstate(void);
 
-uint8_t packet_recv(struct ubpacket*);
-uint8_t packet_recvstate(void);
+//uint8_t packet_recv(struct ubpacket*);
+//uint8_t packet_recvstate(void);
 
 void packet_tick(void);
 inline struct ubpacket * packet_getSendBuffer(void);
 void packet_send(void);
 uint8_t packet_done(void);
+inline uint8_t packet_gotPacket(void);
+inline void packet_processed(void);
+inline struct ubpacket * packet_getIncomming(void);
+
 #define UB_PACKET_IDLE         0
 #define UB_PACKET_BUSY          1
 #define UB_PACKET_TIMEOUT       2
