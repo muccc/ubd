483565f9663b3b01cc364f6bc62c01d9f26c0bfe
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2009-09-21 02:14:40 +0000

    added testprojects
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@95 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

---
+++ firmware2/testclient/ubtest.c
@@ -0,0 +1,43 @@
+//#include "ubconfig.h"
+#include "ub.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include "ubstat.h"
+#include "ubrs485slave.h"
+#include "usart.h"
+#include "serial_handler.h"
+
+volatile uint8_t gotms = 0;
+ISR(TIMER0_COMPA_vect)
+{
+    gotms = 1;
+}
+
+int main(void)
+{
+    uint16_t i = 0;
+    DDRA = 0xFF;
+    
+    TCCR0A = (1<<WGM01);
+    TCCR0B = (1<<CS02) | (1<<CS00);
+    OCR0A=17;       //~1ms  
+    TIMSK0|=(1<<OCIE0A);
+
+    uart1_init( UART_BAUD_SELECT(115200,F_CPU));
+
+    ub_init(UB_SLAVE);
+    ub_setAddress(0x10);
+    sei();
+    uint8_t c[] = "hello";
+    while(1){
+        ub_process();
+        if( gotms ){
+            gotms = 0;
+            if(i++==500){
+                i = 0;
+            }
+            ub_tick();
+        }
+    }
+}
---
+++ firmware2/testmaster/ubtest.c
@@ -0,0 +1,61 @@
+//#include "ubconfig.h"
+#include "ub.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+#include "ubstat.h"
+#include "ubrs485master.h"
+#include "usart.h"
+#include "serial_handler.h"
+
+volatile uint8_t gotms = 0;
+ISR(TIMER0_COMPA_vect)
+{
+    gotms = 1;
+}
+
+int main(void)
+{
+    uint16_t i = 0;
+    DDRA = 0xFF;
+    
+    TCCR0A = (1<<WGM01);
+    TCCR0B = (1<<CS02) | (1<<CS00);
+    OCR0A=17;       //~1ms  
+    TIMSK0|=(1<<OCIE0A);
+
+    uart1_init( UART_BAUD_SELECT(115200,F_CPU));
+
+    ub_init(UB_MASTER);
+
+    ubstat_addNode(45,0xFF);
+    ubstat_addNode(55,0xFF);
+    ubstat_addNode(95,0xFF);
+    sei();
+    uint8_t c[10];
+    c[0] = 'h';
+    c[1] = 'e';
+    c[2] = 'l';
+    c[3] = 'l';
+    c[4] = '0';
+    c[5] = 0;
+
+    while(1){
+        ub_process();
+        uint8_t packet[30];
+        uint8_t len = rs485master_getPacket(packet);
+        if( len ){
+            serial_putenc(packet,len);
+        }
+        if( gotms ){
+            gotms = 0;
+            if(i++==500){
+                i = 0;
+                if( rs485master_send(c, 6) == UB_OK )
+                    c[5]++;
+            }
+            ub_tick();
+        }
+        
+    };
+}
