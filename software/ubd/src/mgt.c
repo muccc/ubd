5651540d8c59f314f0eeb5adc60a73561c9a017c
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2010-04-09 08:21:19 +0000

    created registernode function check netadr when searching a node set the name from the id
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@133 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/ubd/src/mgt.c
+++ software/ubd/src/mgt.c
@@ -7,13 +7,16 @@
 #include "address6.h"
 #include "ubpacket.h"
 #include "interface.h"
+#include "db.h"
 
 struct node nodes[MAX_NODE];
 
-//static address_t mgt_getFreeAddress(void);
 static struct node* mgt_getFreeNode(void);
 static void mgt_checkTimeout(void);
 static gboolean mgt_tick(gpointer data);
+static void mgt_setNameFromID(struct node *n);
+static void mgt_registerNode(struct node * n, char *id,
+                             uint8_t type, uint8_t busadr);
 
 void mgt_init(void)
 {
@@ -24,24 +27,8 @@ void mgt_init(void)
     }
 
     nodes[2].type = TYPE_BRIDGE;       //this node is reserved
+    nodes[2].netadr = NULL;       //this node is reserved
     g_timeout_add_seconds(1,mgt_tick,NULL);
-}
-
-static void mgt_checkTimeout(void)
-{
-    gint i;
-    for(i=4; i<MAX_NODE; i+=1){
-        if( nodes[i].state == NODE_IDENTIFY ||
-            nodes[i].state == NODE_NORMAL ){
-            if( nodes[i].timeout-- == 0 ){
-                printf("removing %s\n",nodes[i].id);
-                net_removeSockets(&nodes[i]);
-                address6_removeAddress(&nodes[i]);
-                //TODO: remove sockets too
-                nodes[i].type = TYPE_NONE;
-            }
-        }
-    }
 }
 
 static gboolean mgt_tick(gpointer data)
@@ -61,10 +48,7 @@ struct node *mgt_createNode(gint type, gchar *id)
 {
     struct node *n =  mgt_getFreeNode();
     if( n != NULL ){
-        n->type = type;
-        strncpy(n->id,id,MAX_ID);
-        address6_createAddress(n);
-        n->netup = FALSE;
+        mgt_registerNode(n, id, type, 0);
     }
     return n;
 }
@@ -72,11 +56,7 @@ struct node *mgt_createNode(gint type, gchar *id)
 struct node *mgt_createBridge(gchar *id)
 {
     struct node *n = &nodes[2];
-    n->busadr = 2;
-    n->type = TYPE_BRIDGE;
-    strncpy(n->id,id,MAX_ID);
-    address6_createAddress(n);
-    n->netup = FALSE;
+    mgt_registerNode(n, id, TYPE_BRIDGE, 2);
     return n;
 }
 
@@ -104,7 +84,6 @@ struct node* mgt_getNodeByBusAdr(gint adr)
             }
         }
     }
-//    printf("mgt: getnodebyname: node %s unknown\n",name);
     return NULL;
 }
 
@@ -112,7 +91,7 @@ struct node* mgt_getNodeByNetAdr(GInetAddress *addr)
 {
     gint i;
     for(i=0; i<MAX_NODE; i++){
-        if( nodes[i].type != TYPE_NONE ){
+        if( nodes[i].type != TYPE_NONE && nodes[i].netadr != NULL ){
             if( g_inet_address_get_native_size(addr) == 
                 g_inet_address_get_native_size(nodes[i].netadr) &&
                 memcmp(g_inet_address_to_bytes(addr),
@@ -122,20 +101,42 @@ struct node* mgt_getNodeByNetAdr(GInetAddress *addr)
             }
         }
     }
-//    printf("mgt: getnodebyname: node %s unknown\n",name);
     return NULL;
 }
 
-/*address_t mgt_getFreeAddress(void)
+static void mgt_registerNode(struct node * n, char *id, uint8_t type, uint8_t busadr)
 {
-    gint i;
-    for(i=4; i<MAX_NODE; i+=1){
-        if( nodes[i].type == TYPE_NONE){
-            return i;
-        }
+    n->type = type;
+    if( busadr )
+        n->busadr = busadr;
+    strncpy(n->id,id,MAX_ID);
+
+    if( db_isNodeKnown(n->id) ){
+        printf("node known\n");
+        db_loadNode(n);
+        address6_createAddress(n);
+    }else{
+        printf("node unknown\n");
+        n->netup = FALSE;
+        memset(n->groups,0,sizeof(n->groups));
+        n->netadr = NULL;
+        mgt_setNameFromID(n);
+        address6_createAddress(n);
+        db_addNode(n);
     }
-    return 0;
-}*/
+}
+
+static void mgt_setNameFromID(struct node *n)
+{ 
+    memcpy(n->name,n->id,MAX_ID);
+    char *s = strchr(n->name,',');
+
+    if( s != NULL){
+        *s = 0;
+    }else{
+        printf("ill formated id for this node: %s\n",n->id);
+    }
+}
 
 static struct node* mgt_getFreeNode(void)
 {
@@ -149,10 +150,29 @@ static struct node* mgt_getFreeNode(void)
     return NULL;
 }
 
-gint mgt_removeEntryById(gchar * id)
+static void mgt_checkTimeout(void)
+{
+    gint i;
+    for(i=4; i<MAX_NODE; i+=1){
+        if( nodes[i].state == NODE_IDENTIFY ||
+            nodes[i].state == NODE_NORMAL ){
+            if( nodes[i].timeout-- == 0 ){
+                printf("removing %s\n",nodes[i].id);
+                net_removeSockets(&nodes[i]);
+                address6_removeAddress(&nodes[i]);
+                //TODO: remove sockets too
+                nodes[i].type = TYPE_NONE;
+            }
+        }
+    }
+}
+
+
+
+/*gint mgt_removeEntryById(gchar * id)
 {
     id = NULL;
-/*    GSequenceIter * i;
+    GSequenceIter * i;
     for( i=g_sequence_get_begin_iter(entries);
             i!=g_sequence_get_end_iter(entries); 
             i=g_sequence_iter_next(i) ){
@@ -163,7 +183,6 @@ gint mgt_removeEntryById(gchar * id)
             g_sequence_remove(i);
             return 0;
         }
-    }*/
+    }
     return 1;
-}
-
+}*/
