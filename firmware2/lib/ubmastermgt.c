fa9f0cfb483f1da4ea155dc3169517d385ea7718
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2010-04-16 09:38:59 +0000

    cleaned up master mgt
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@135 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- firmware2/lib/ubmastermgt.c
+++ firmware2/lib/ubmastermgt.c
@@ -28,15 +28,15 @@ void ubmastermgt_init(void)
 
 uint8_t ubmastermgt_process(struct ubpacket_t * p)
 {
-    uint8_t * d = p->data;
+    uint8_t * data = p->data;
     struct ubstat_t * flags;
     struct ubpacket_t * out;
     if(!(p->header.flags & UB_PACKET_MGT))
         return 0;
-    switch(d[0]){
+    switch(data[0]){
         case 'q':
-            flags = ubstat_getFlags(d[1]);
-            rs485master_setQueryInterval(d[1], (d[2] << 8) + d[3]);
+            flags = ubstat_getFlags(data[1]);
+            rs485master_setQueryInterval(data[1], (data[2] << 8) + data[3]);
             flags->known = 1;
             //TODO: check if it'ss on the rf
             flags->rs485 = 1;
@@ -46,15 +46,22 @@ uint8_t ubmastermgt_process(struct ubpacket_t * p)
             ubmastermgt_state = CONNECTED;
         break;
         case 'r':
-            //TODO: get the reset to the other nodes too
-            while(1);
+            //XXX: think about implementing this again
+            //why did this get removed?
+            //while(1);
+        break;
+        case 1:
+            ubadr_addMulticast(data[1]);
+        break;
+        case 2:
+            ubadr_removeMulticast(data[1]);
         break;
         case 'V':
             out = ubpacket_getSendBuffer();
             out->header.dest = UB_ADDRESS_MASTER;
             out->header.src = ubadr_getAddress();
             out->header.flags = UB_PACKET_MGT;
-            sprintf((char *)out->data,"D="__DATE__);
+            sprintf((char *)out->data,"V="__DATE__);
             out->header.len = strlen((char*)out->data);
             ubpacket_send();
         break;
@@ -66,33 +73,37 @@ void ubmastermgt_tick(void)
 {
     struct ubpacket_t * p;
     static uint16_t time = 0;
+    static uint8_t flags = 0;
     if(!time--){
         time = 1000;
-    }
-
-    if(time == 0){
-        p = ubpacket_getSendBuffer();
         switch(ubmastermgt_state){
             case DISCOVER:
-                p->header.src = ubadr_getAddress();
-                p->header.dest = UB_ADDRESS_MASTER;
-                p->header.flags = UB_PACKET_MGT | UB_PACKET_NOACK;
-                p->data[0] = MGT_BRIDGEDISCOVER;
-                strcpy((char*)p->data+1,(char*)ubadr_getID());
-                p->header.len = strlen((char*)p->data);
-                ubpacket_send();
+                flags |= 0x01;
             break;
             case CONNECTED:
-                if( ubpacket_free() ){
-                    p->header.dest = UB_ADDRESS_MASTER;
-                    p->header.src = ubadr_getAddress();
-                    p->header.flags = UB_PACKET_MGT;
-                    p->data[0] = MGT_MASTERALIVE;
-                    p->header.len = 1;
-                    ubpacket_send();
-                }
+                flags |= 0x02;
             break;
         }
+    }
+
+    if(flags && ubpacket_free()){
+        p = ubpacket_getSendBuffer();
+        p->header.src = ubadr_getAddress();
+        p->header.dest = UB_ADDRESS_MASTER;
+
+        if( flags & 0x01 ){
+            flags ^= 0x01;
+            p->header.flags = UB_PACKET_MGT | UB_PACKET_NOACK;
+            p->data[0] = MGT_BRIDGEDISCOVER;
+            strcpy((char*)p->data+1,(char*)ubadr_getID());
+            p->header.len = strlen((char*)p->data);
+        }else if( flags & 0x02 ){
+            flags ^= 0x02;
+            p->header.flags = UB_PACKET_MGT;
+            p->data[0] = MGT_MASTERALIVE;
+            p->header.len = 1;
+        }
+        ubpacket_send();
     }
 }
 
