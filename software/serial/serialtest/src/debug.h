982d047c4485c2fa47ee46268b297063a323b6ea
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2010-04-16 09:59:59 +0000

    removed old code
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@154 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/serial/serialtest/src/debug.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DEBUG_H_
-#define __DEBUG_H_
-#include <stdint.h>
-
-void debug_hexdump(uint8_t * data, uint16_t len);
-#endif
--- software/serial/serialtest/src/net6.h
+++
@@ -1,7 +0,0 @@
-#ifndef __NET6_H__
-#define __NET6_H__
-#include <gnet.h>
-
-gint net_init(gchar* iface);
-
-#endif
