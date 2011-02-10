4d0e72be2aea41c0023349fe9dddbc5e29801e01
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2011-02-10 22:26:34 +0000

    time parameter for fadeexample and blink
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@325 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/pythonlib/blink.py
+++ software/pythonlib/blink.py
@@ -5,6 +5,7 @@ import sys
 
 lamp = sys.argv[1]
 a = libml.Moodlamp(lamp)
+t = float(sys.argv[2])
 #a = libml.Moodlamp("2001:a60:e801:1001:1::3")
 
 #if a.connect() != True:
@@ -13,7 +14,7 @@ a = libml.Moodlamp(lamp)
 a.connect();
 while 1:
     a.setcolor(0,255,255)
-    time.sleep(0.5)
+    time.sleep(t)
     a.setcolor(0,0,0)
-    time.sleep(0.5)
+    time.sleep(t)
 
--- software/pythonlib/fadeexample.py
+++ software/pythonlib/fadeexample.py
@@ -4,13 +4,15 @@ import time
 import sys
 
 lamp = sys.argv[1]
+t = float(sys.argv[2])
+
 a = libml.Moodlamp(lamp)
 a.connect();
 while 1:
-    a.timedfade(0,0,255,3)
-    time.sleep(3)
-    a.timedfade(0,255,0,3)
-    time.sleep(3)
-    a.timedfade(255,0,0,3)
-    time.sleep(3)
+    a.timedfade(0,0,255,t)
+    time.sleep(t)
+    a.timedfade(0,255,0,t)
+    time.sleep(t)
+    a.timedfade(255,0,0,t)
+    time.sleep(t)
 
