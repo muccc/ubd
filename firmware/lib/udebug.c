ff7be17d5b611283a7dfccd5ef113d3e8f769190
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2011-03-03 23:46:25 +0000

    udebug is inactive
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@368 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- firmware/lib/udebug.c
+++ firmware/lib/udebug.c
@@ -3,10 +3,10 @@
 void udebug_init(void)
 {
     //DDRC |= (1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3);
-    DDRC |= (1<<PC0) | (1<<PC1);
+    //DDRC |= (1<<PC0) | (1<<PC1);
     //PORTC &= ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3)); 
-    PORTC &= ~((1<<PC0) | (1<<PC1)); 
-    PORTC |= (1<<PC2);
+    //PORTC &= ~((1<<PC0) | (1<<PC1)); 
+    //PORTC |= (1<<PC2);
 }
 
 inline void udebug_edge(void)
