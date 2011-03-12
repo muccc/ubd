295fa9897a8bca08ba180e15cf0b96d783558085
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2011-03-12 20:41:02 +0000

    updated alarm
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@405 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/pythonlib/switchsiren.py
+++ software/pythonlib/switchsiren.py
@@ -19,34 +19,43 @@ a.connect()
 s.connect()
 s.listen()
 state = 'b'
+play = False
 pygame.mixer.init()
 music.load('siren.wav')
 
 def setcolors():
     global state
+    global play
     while 1:
-        if state == 'b':
+        if state == 'b0':
             a.timedfade(0,0,255,1)
-            time.sleep(1);
+            music.stop()
+            play = False
+            state = 'b1'
+        elif state == 'b1':
+            time.sleep(.1)
         elif state == 'r1':
             a.timedfade(255,0,0,.3)
+            if not play:
+            	music.play(-1)
+                play = True
             state = 'r2'
             time.sleep(.3);
         elif state == 'r2':
             a.timedfade(0,0,0,.3)
             state = 'r1'
-            time.sleep(.3);
+            time.sleep(.3)
 
 thread.start_new_thread(setcolors,())
 
 while True:
+    #time.sleep(1)
+    #continue
     rc = s.receiveStatus()
     if rc == 'b0':
-        state = 'b'
-        music.stop()
+        state = 'b0'
         #a.timedfade(0,0,255,1)
     elif rc == 'B0':
         state = 'r1'
-        music.play(-1)
         #a.timedfade(255,0,0,1)
 
