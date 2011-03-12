69ce3036bf45cea331a6d4ea6980b78ec52f325c
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2011-03-12 20:46:33 +0000

    alarm: read color from one lamp
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@407 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/pythonlib/switchsiren.py
+++ software/pythonlib/switchsiren.py
@@ -9,26 +9,31 @@ import pygame
 from pygame.mixer import music
 
 
-lamp = sys.argv[1]
+lamps = sys.argv[1]
 switch = sys.argv[2]
+lamp = sys.argv[3]
 
-a = libml.Moodlamp(lamp,True)
+a = libml.Moodlamp(lamps,True)
 s = libswitch.Switch(switch)
+l = libml.Moodlamp(lamp)
 
 a.connect()
 s.connect()
+l.connect()
+
 s.listen()
 state = 'b'
 play = False
 pygame.mixer.init()
 music.load('siren.wav')
+r = g = b = 0
 
 def setcolors():
-    global state
+    global state, r, g, b
     global play
     while 1:
         if state == 'b0':
-            a.timedfade(0,0,255,1)
+            a.timedfade(r,g,b,1)
             music.stop()
             play = False
             state = 'b1'
@@ -56,6 +61,10 @@ while True:
         state = 'b0'
         #a.timedfade(0,0,255,1)
     elif rc == 'B0':
+        color = l.getcolor()
+        r = ord(color[0])
+        g = ord(color[1])
+        b = ord(color[2])
         state = 'r1'
         #a.timedfade(255,0,0,1)
 
