7e34813cf002d4c30eeb700d06393627bc0fc00e
schneider <schneider@0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6>
2011-03-29 15:01:28 +0000

    ubd: cleanup syslog hexdump
    git-svn-id: https://brezn.muc.ccc.de/svn/uberbus/trunk@461 0ae26b70-98e1-47eb-8c5c-8e5c6e8157e6

--- software/ubd/src/debug.c
+++ software/ubd/src/debug.c
@@ -14,16 +14,25 @@
 
 void debug_hexdump(uint8_t * data, uint16_t len)
 {
-    uint16_t i;
+    uint16_t i,j;
+    char *buf = malloc(len*5);
+    if( buf == NULL ){
+        syslog(LOG_ERR,"debug_hexdump: malloc failed");
+        return;
+    }
+
+    j = 0;
     for(i=0; i<len; i++){
         if( data[i] < 0x10 ){
-            syslog(LOG_DEBUG," 0x0%X", data[i]);
+            j += sprintf(buf+j, " 0x0%X", data[i]);
         }else if (data[i] < ' ' || data[i] > 0x7F){
-            syslog(LOG_DEBUG," 0x%X", data[i]);
+            j += sprintf(buf+j, " 0x%X", data[i]);
         }else{
-            syslog(LOG_DEBUG,"%c", data[i]);
+            j += sprintf(buf+j, "%c", data[i]);
         }
     }
+    syslog(LOG_DEBUG, "%s", buf);
+    free(buf);
 }
 
 void debug_packet(gchar *reporter, struct ubpacket* p)
@@ -49,11 +58,10 @@ void debug_packet(gchar *reporter, struct ubpacket* p)
         strcat(flags, "UNSOLICITED | ");
    
     g_get_current_time(&t);
-    syslog(LOG_DEBUG,"%ld.%04ld ",t.tv_sec,t.tv_usec);
-    syslog(LOG_DEBUG,"%s: packet from %u to %u flags: (%x: %s) class: %u len: %u: ",
+    syslog(LOG_DEBUG,"%ld.%04ld %s: packet from %u to %u flags: (%x: %s) class: %u len: %u: ",
+            t.tv_sec, t.tv_usec,
             reporter, p->src, p->dest, p->flags, flags, p->class,p->len);
     debug_hexdump(p->data, p->len);
-    syslog(LOG_DEBUG,"\n");
 }
 
 
