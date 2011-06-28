ed918bb045327687fecc4b0645a07021a3c5c11b
schneider <schneider@blinkenlichts.net>
2011-06-28 21:47:14 +0200

    forgot some files

---
+++ firmware/moodlamp-rf/busconfig.h
@@ -0,0 +1,10 @@
+#ifndef _BUSCONFIG_H_
+#define _BUSCONFIG_H_
+
+#define UB_ENABLESLAVE 
+#define UB_ENABLEBRIDGE
+
+#define UB_INTERVAL         500
+#define UB_CLASSES          {UB_CLASS_MOODLAMP,0,0,0}
+
+#endif
---
+++ firmware/moodlamp-rf/deviceconfig.h
@@ -0,0 +1,12 @@
+#ifndef _DEVICECONFIG_H_
+#define _DEVICECONFIG_H_
+
+//#define USEDEBUG
+#define BOARD_MOODLAMP
+
+#define UB_ENABLERF 
+#define RF_CHANNEL  23
+#define UB_ENABLERS485 
+
+#endif
+
