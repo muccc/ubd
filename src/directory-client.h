650a42656782cd2e343316ff95527775e6c7ac50
schneider <schneider@blinkenlichts.net>
2011-11-15 10:56:42 +0100

    added files for directory support

---
+++ src/directory-client.h
@@ -0,0 +1,7 @@
+#ifndef _DIRECTORY_CLIENT_H_
+#define _DIRECTORY_CLIENT_H_
+
+void dirclient_init(void);
+void dirclient_addService(void);
+void dirclient_removeService(void);
+#endif
---
+++ src/directory-server.h
@@ -0,0 +1,6 @@
+#ifndef _DIRECTORY_SERVER_H_
+#define _DIRECTORY_SERVER_H_
+
+void dirserver_init(void);
+
+#endif
